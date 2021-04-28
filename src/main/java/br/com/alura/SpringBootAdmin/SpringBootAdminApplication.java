3bb6ff15f233001fde2d63323ca94550b0ee675b
Study Profile <dee.alves2@gmail.com>
2021-04-27 22:49:10 -0300

    Criando Projeto Spring-Boot-Admin

---
+++ src/main/java/br/com/alura/SpringBootAdmin/SpringBootAdminApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.SpringBootAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+
+}
